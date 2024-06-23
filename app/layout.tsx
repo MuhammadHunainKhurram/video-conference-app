90154cf4f5a5b321b9314a5de2fe5407683ab97a
Muhammad <mhk8367@mavs.uta.edu>
2024-06-23 00:42:22 -0500

    fixed imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from '@/components/ui/toaster';
 import '@stream-io/video-react-sdk/dist/css/styles.css';
